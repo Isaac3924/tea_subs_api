9ad8e6eb618b258d19be876eb44a7acd71592be2
Isaac Alter <i_alter@yahoo.com>
2023-06-01 03:00:18 -0600

    Add refactor: refactored customer spec to properly check validations and relationships. Might refactor further to remove premade testing customers, teas, and subs as contemplating current design, wondering if methods will be needed in model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :first_name }
-    it { should validate_presence_of :last_name }
-    it { should validate_presence_of :email }
-    it { should validate_presence_of :address }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:address) }
   end
 
   describe 'relationships' do
-    it { should have_many :subscriptions }
+    it { should have_and_belong_to_many :subscriptions }
   end
 
   before :each do
+    # Create customers to test
     @customer1 = Customer.create!(
                                   first_name: 'Bob', 
                                   last_name: 'Gu', 
@@ -25,6 +26,7 @@ RSpec.describe Customer, type: :model do
                                   email: 'sue@gmail.com',
                                   address: '456 Main St'
                                 )
+    # Create teas to test
     @tea1 = Tea.create!(
                         title: 'Green Tea',
                         description: 'Green tea is made from Camellia sinensis leaves that have not undergone the same withering and oxidation applied when processing Camellia sinensis into oolong tea and black tea.',
@@ -43,7 +45,32 @@ RSpec.describe Customer, type: :model do
                         temperature: 190,
                         brew_time: 4
                       )
+    # Create subscriptions to test
     @subscription1 = Subscription.create!(
-                                          title: 'Green Tea Subscription',
+                                          title: 'Green & Black Subscription',
+                                          price: 15.99,
+                                          status: "active",
+                                          frequency: 1
+                                        )
+    @subscription2 = Subscription.create!(
+                                          title: 'Oolong & Green Subscription',
+                                          price: 21.99,
+                                          status: "cancelled",
+                                          frequency: 1
+                                        )
+    @subscription3 = Subscription.create!(
+                                          title: 'All Three Subscription',
+                                          price: 1.99,
+                                          status: "active",
+                                          frequency: 1
+                                        )
+    # Assign teas to subscriptions
+    @subscription1.teas << [@tea1, @tea2]
+    @subscription2.teas << [@tea3, @tea1]
+    @subscription3.teas << [@tea1, @tea2, @tea3]
+
+    # Assign subscriptions to customers
+    @customer1.subscriptions << [@subscription1, @subscription2]
+    @customer2.subscriptions << [@subscription3]
   end
 end
