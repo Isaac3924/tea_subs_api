3e46f826abc0d5b198b445d95cf8d7b6fe509f47
Isaac Alter <i_alter@yahoo.com>
2023-06-02 11:44:06 -0600

    Add refactor: new logic to deal with sadpath testing and errors that could arise, specifically with improper json body provided and not present customers or subscriptions

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -2,15 +2,23 @@ class Api::V1::SubscriptionsController < ApplicationController
   before_action :set_customer, only: %i[create index]
 
   def create
-    @subscription = @customer.subscriptions.create!(subscription_params)
-    @subscription.teas << Tea.find(subscription_params[:tea_ids])
-    render json: SubscriptionSerializer.format_subscription(@subscription), status: 201
+    @subscription = @customer.subscriptions.create(subscription_params)
+    if @subscription.save
+      @subscription.teas << Tea.find(subscription_params[:tea_ids])
+      render json: SubscriptionSerializer.format_subscription(@subscription), status: 201
+    else
+      render json: { error: "Unprocessable" }, status: 422
+    end
   end
 
   def update
-    @subscription = Subscription.find(params[:id])
-    @subscription.update!(status: 'cancelled')
-    render json: SubscriptionSerializer.format_subscription(@subscription), status: 200
+    begin
+      @subscription = Subscription.find(params[:id])
+      @subscription.update!(status: 'cancelled')
+      render json: SubscriptionSerializer.format_subscription(@subscription), status: 200
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Subscription not found" }, status: 404
+    end
   end
 
   def index
@@ -21,7 +29,11 @@ class Api::V1::SubscriptionsController < ApplicationController
   private
 
   def set_customer
-    @customer = Customer.find(params[:customer_id])
+    begin
+      @customer = Customer.find(params[:customer_id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Customer not found" }, status: 404
+    end
   end
 
   def subscription_params
--- spec/requests/api/v1/subscription_request_spec.rb
+++ spec/requests/api/v1/subscription_request_spec.rb
@@ -13,55 +13,127 @@ RSpec.describe 'Subscriptions', type: :request do
         }
       }
     end
+
+    let(:invalid_json) do
+      {
+        "customer_id": 1,
+        "subscriptions": {
+          "title": "All Three Subscription",
+          "status": "active",
+          "frequency": "one",
+          "tea_ids": [1, 2, 3]
+        }
+      }
+    end
     
     context 'when the request is valid' do
-      before do
-        Rails.application.load_seed
+      describe 'POST /api/v1/customers/:customer_id/subscriptions' do
+        before do
+          Rails.application.load_seed
+        end
+        before { post '/api/v1/customers/1/subscriptions', params: valid_json, as: :json }
+  
+        it "creates a new subscription" do
+          pretty = JSON.parse(response.body)
+  
+          expect(response).to have_http_status(201)
+          expect(pretty["data"]["attributes"]["title"]).to eq("All Three Subscription")
+          expect(pretty["data"]["attributes"]["price"]).to eq(15.99)
+          expect(pretty["data"]["attributes"]["status"]).to eq("active")
+          expect(pretty["data"]["attributes"]["frequency"]).to eq("one")
+        end
       end
-      before { post '/api/v1/customers/1/subscriptions', params: valid_json, as: :json }
 
-      it "creates a new subscription" do
-        pretty = JSON.parse(response.body)
+      describe 'PATCH /api/v1/subscriptions/:id' do
+        before do
+          Rails.application.load_seed
+        end
+        before { patch '/api/v1/subscriptions/1' }
+    
+        it "updates subscription status to cancelled" do
+          pretty = JSON.parse(response.body)
+    
+          expect(response).to have_http_status(200)
+          expect(pretty["data"]["attributes"]["status"]).to eq("cancelled")
+        end
+      end
 
-        expect(response).to have_http_status(201)
-        expect(pretty["data"]["attributes"]["title"]).to eq("All Three Subscription")
-        expect(pretty["data"]["attributes"]["price"]).to eq(15.99)
-        expect(pretty["data"]["attributes"]["status"]).to eq("active")
-        expect(pretty["data"]["attributes"]["frequency"]).to eq("one")
+      describe 'GET /api/v1/customers/:customer_id/subscriptions' do
+        before do
+          Rails.application.load_seed
+        end
+        before { get '/api/v1/customers/2/subscriptions' }
+    
+        it "updates subscription status to cancelled" do
+          pretty = JSON.parse(response.body)
+    
+          expect(response).to have_http_status(200)
+          expect(pretty["subscription_list"]).to be_an(Array)
+          expect(pretty["subscription_list"].count).to eq(2)
+          expect(pretty["subscription_list"][0]["data"]["attributes"]["title"]).to eq("Green & Black Subscription")
+          expect(pretty["subscription_list"][1]["data"]["attributes"]["title"]).to eq("Oolong & Green Subscription")
+          expect(pretty["subscription_list"][0]["data"]["attributes"]["status"]).to eq("active")
+          expect(pretty["subscription_list"][1]["data"]["attributes"]["status"]).to eq("cancelled")
+        end
       end
     end
-  end
 
-  describe 'PATCH /api/v1/subscriptions/:id' do
-    before do
-      Rails.application.load_seed
-    end
-    before { patch '/api/v1/subscriptions/1' }
+    context 'when the request is invalid' do
+      describe 'POST /api/v1/customers/:customer_id/subscriptions' do
+        before do
+          Rails.application.load_seed
+        end
+        before { post '/api/v1/customers/1/subscriptions', params: invalid_json, as: :json }
+  
+        it "returns a 422 error" do
+          pretty = JSON.parse(response.body)
+  
+          expect(response).to have_http_status(422)
+          expect(pretty["error"]).to eq("Unprocessable")
+        end
+      end
 
-    it "updates subscription status to cancelled" do
-      pretty = JSON.parse(response.body)
+      describe 'POST /api/v1/customers/:invalid_customer_id/subscriptions' do
+        before do
+          Rails.application.load_seed
+        end
+        before { post '/api/v1/customers/9999/subscriptions', params: valid_json, as: :json }
+  
+        it "returns a 404 error" do
+          pretty = JSON.parse(response.body)
+          
+          expect(response).to have_http_status(404)
+          expect(pretty["error"]).to eq("Customer not found")
+        end
+      end
 
-      expect(response).to have_http_status(200)
-      expect(pretty["data"]["attributes"]["status"]).to eq("cancelled")
-    end
-  end
+      describe 'PATCH /api/v1/subscriptions/:invalid_id' do
+        before do
+          Rails.application.load_seed
+        end
+        before { patch '/api/v1/subscriptions/99999' }
+    
+        it "returns a 404 error" do
+          pretty = JSON.parse(response.body)
+    
+          expect(response).to have_http_status(404)
+          expect(pretty["error"]).to eq("Subscription not found")
+        end
+      end
 
-  describe 'GET /api/v1/customers/2/subscriptions' do
-    before do
-      Rails.application.load_seed
-    end
-    before { get '/api/v1/customers/2/subscriptions' }
-
-    it "updates subscription status to cancelled" do
-      pretty = JSON.parse(response.body)
-
-      expect(response).to have_http_status(200)
-      expect(pretty["subscription_list"]).to be_an(Array)
-      expect(pretty["subscription_list"].count).to eq(2)
-      expect(pretty["subscription_list"][0]["data"]["attributes"]["title"]).to eq("Green & Black Subscription")
-      expect(pretty["subscription_list"][1]["data"]["attributes"]["title"]).to eq("Oolong & Green Subscription")
-      expect(pretty["subscription_list"][0]["data"]["attributes"]["status"]).to eq("active")
-      expect(pretty["subscription_list"][1]["data"]["attributes"]["status"]).to eq("cancelled")
+      describe 'GET /api/v1/customers/:invalid_customer_id/subscriptions' do
+        before do
+          Rails.application.load_seed
+        end
+        before { get '/api/v1/customers/9999/subscriptions' }
+    
+        it "returns a 404 error" do
+          pretty = JSON.parse(response.body)
+    
+          expect(response).to have_http_status(404)
+          expect(pretty["error"]).to eq("Customer not found")
+        end
+      end
     end
   end
 end
